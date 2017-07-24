1a4a537ce7fed0f6b85b7829be30c7268b82b150
benoit <krazylek@gmail.com>
2017-07-24 16:18:55 +1100

    unamed func

--- template.js
+++ template.js
@@ -1,62 +1,46 @@
 var html = require('bel')
 
 module.exports = {
-  headerContent: headerContent,
-  dateContent: dateContent,
-  containerTemplate: containerTemplate,
-  headerTemplate: headerTemplate,
-  dateTemplate: dateTemplate,
-}
+  headerContent: function(date) { 
+    return html`<span>${date.toString().slice(0,3)}</span>` 
+  },
+  headerTemplate: function(content, options) { 
+    if(options.noHeader)
+      return
+    return html`<th>${content}</th>`
+  },
+  dateContent: function(date) { 
+    return html`<span>${date.getDate()}</span>` 
+  },
+  dateTemplate: function(content, options)  { 
+    return function(date, info)  { 
+      var classList = []
+      var day = info.dayOfMonth
 
-function headerTemplate(content, options) { 
-  if(options.noHeader)
-    return
-  return html`<th>${content}</th>`
-}
+      if(info.siblingMonth)
+        classList.push(options.classPrefix + 'inactive')
+      if(options.selected && options.selected.hasOwnProperty(day)) {
+        classList = classList.concat(options.selected[day])
+      }
 
-function headerContent(date) { 
-  return html`<span>${date.toString().slice(0,3)}</span>` 
-}
+      var element = html`<td>${content}</td>`
+      if(classList.length)
+        element.className = classList.join(' ')
 
-function dateTemplate(content, options)  { 
-  return function(date, info)  { 
-    var classList = []
-    var day = info.dayOfMonth
-
-    if(info.siblingMonth)
-      classList.push(options.classPrefix + 'inactive')
-    if(options.selected && options.selected.hasOwnProperty(day)) {
-      classList = classList.concat(options.selected[day])
+      return element
     }
-
-    var element = html`<td>${content}</td>`
-    if(classList.length)
-      element.className = classList.join(' ')
-
-    return element
-  }
-}
-
-function dateContent(date) { 
-  return html`<span>${date.getDate()}</span>` 
-}
-
-function containerTemplate(month, options)  { 
-  var className = options.classPrefix + 'block'
-  var headers = html`
-    <tr>
-      ${month[0]}
-    </tr>`
-  var weeks = month.slice(1).map(function(row) {
-    return html`
-      <tr>
-        ${row}
-      </tr>`
+  },
+  containerTemplate: function(month, options)  { 
+    var className = options.classPrefix + 'block'
+    var headers = html`<tr>${month[0]}</tr>`
+    var weeks = month.slice(1).map(function(row) {
+      return html`<tr>${row}</tr>`
     })
 
-  return html`
-    <table class="${className}">
-      ${options.noHeader ? null : headers} 
-      ${weeks}
-    </table>`
+    return html`
+      <table class="${className}">
+        ${options.noHeader ? null : headers} 
+        ${weeks}
+      </table>`
+  }
 }

0c631843305cd638a659b693a6ce77db6c0826d2
benoit <krazylek@gmail.com>
2017-10-31 12:41:15 +1100

    add source link to examples

--- example/index.js
+++ example/index.js
@@ -1,6 +1,11 @@
 var html = require('bel')
 var css = require('sheetify')
 
+function sourceLink(example) {
+  var href = `https://github.com/krazylek/calendar-month-view/tree/master/example/${example}/index.js` 
+  return html`<p><a href="${href}">View sources</a></p>`
+}
+
 css('./page.css')
 
 function render() {
@@ -9,10 +14,19 @@ function render() {
       <h1>calendar-month-view examples</h1>
          
       ${require('./basic')()}
+      ${sourceLink('basic')}
+
       ${require('./selection')()}
+      ${sourceLink('selection')}
+      
       ${require('./marks')()}
+      ${sourceLink('marks')}
+      
       ${require('./i18n')()}
+      ${sourceLink('i18n')}
+      
       ${require('./custom-container')()}
+      ${sourceLink('custom-container')}
 
       <footer>
         <p>Contribute on <a href="https://github.com/krazylek/calendar-month-view">Github</a></p>
@@ -22,3 +36,4 @@ function render() {
 }
 
 document.body = render()
+
